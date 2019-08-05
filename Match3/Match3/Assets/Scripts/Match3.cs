cd0f0dc9d44e9d55b9b1d544d2d62e803399583f
arxcdr <34399111+arxcdr@users.noreply.github.com>
2019-08-05 19:07:00 +0200

    - Add/Update scripts to generate game board with randomized pieces

--- Match3/Match3/Assets/Scripts/Match3.cs
+++ Match3/Match3/Assets/Scripts/Match3.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Match3 : MonoBehaviour
 {
     public ArrayLayout boardLayout;
+
+    [Header("UI Elements")]
     public Sprite[] pieces;
+    public RectTransform gameBoard;
+
+    [Header("Prefabs")]
+    public GameObject nodePiece;
+
     int width = 9;
     int height = 14;
     Node[,] board;
@@ -15,7 +22,7 @@ public class Match3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartGame();
     }
 
     void StartGame()
@@ -24,6 +31,8 @@ public class Match3 : MonoBehaviour
         random = new System.Random(seed.GetHashCode());
 
         InitializeBoard();
+        VerifyBoard();
+        InstantiateBoard();
     }
 
     void InitializeBoard()
@@ -40,6 +49,7 @@ public class Match3 : MonoBehaviour
 
     void VerifyBoard()
     {
+        List<int> remove;
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
@@ -47,6 +57,36 @@ public class Match3 : MonoBehaviour
                 Point p = new Point(x,y);
                 int val = getValueAtPoint(p);
                 if (val <= 0) continue;
+
+                remove = new List<int>();
+
+                // If we have a match
+                while (isConnected(p, true).Count > 0)
+                {
+                    val = getValueAtPoint(p);
+                    if (!remove.Contains(val))
+                    {
+                        remove.Add(val);
+                    }
+                    setValueAtPoint(p, newValue(ref remove));
+                }
+            }
+        }
+    }
+
+    void InstantiateBoard()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int val = board[x, y].value;
+                if (val <= 0) continue;
+                GameObject p = Instantiate(nodePiece, gameBoard);
+                NodePiece node = p.GetComponent<NodePiece>();
+                RectTransform rect = p.GetComponent<RectTransform>();
+                rect.anchoredPosition = new Vector2(32 + (64 * x), -32 - (64 * y));
+                node.Initialize(val, new Point(x, y), pieces[val - 1]);
             }
         }
     }
@@ -72,7 +112,6 @@ public class Match3 : MonoBehaviour
 
             int same = 0;
             
-
             for(int i=1; i < 3; i++)
             {
                 Point check = Point.add(p, Point.mult(dir, i));
@@ -103,7 +142,7 @@ public class Match3 : MonoBehaviour
             foreach(Point next in check) {
                 if (getValueAtPoint(next) == val)
                 {
-                    line.Add(p);
+                    line.Add(next);
                     same++;
                 }
             }
@@ -133,7 +172,7 @@ public class Match3 : MonoBehaviour
             {
                 if (getValueAtPoint(pnt) == val)
                 {
-                    square.Add(p);
+                    square.Add(pnt);
                     same++;
                 }
             }
@@ -143,11 +182,43 @@ public class Match3 : MonoBehaviour
                 AddPoints(ref connected, square);
             }
         }
-    }
 
+        // Check for other matches on current match
+        if (main)
+        {
+            for(int i=0; i < connected.Count; i++)
+            {
+                AddPoints(ref connected, isConnected(connected[i], false));
+            }
+        }
+
+        // If we have connected pieces, add the main piece
+        if(connected.Count > 0){
+            connected.Add(p);
+        }
+
+        return connected;
+    }
+    
     void AddPoints(ref List<Point> points, List<Point> add)
     {
-
+        foreach(Point p in add)
+        {
+            bool doAdd = true;
+            for(int i = 0; i < points.Count; i++)
+            {
+                // Don't add points if they're already there
+                if (points[i].Equals(p))
+                {
+                    doAdd = false;
+                    break;
+                }
+            }
+            if (doAdd)
+            {
+                points.Add(p);
+            }
+        }
     }
 
     // Return a cube, sphere, cylinder or diamond
@@ -160,7 +231,32 @@ public class Match3 : MonoBehaviour
 
     int getValueAtPoint(Point p)
     {
+        // If out of the array, return a hole just in case
+        if(p.x < 0 || p.x >= width || p.y < 0 || p.y >= height)
+        {
+            return -1;
+        }
         return board[p.x, p.y].value;
+    }
+
+    void setValueAtPoint(Point p, int v)
+    {
+        board[p.x, p.y].value = v;
+    }
+
+    int newValue(ref List<int> remove)
+    {
+        List<int> available = new List<int>();
+        for(int i =0; i < pieces.Length; i++)
+        {
+            available.Add(i + 1);
+        }
+        foreach(int i in remove)
+        {
+            available.Remove(i);
+        }
+        if (available.Count <= 0) return 0;
+        return available[random.Next(0, available.Count)];
     }
 
     // Update is called once per frame
---
+++ Match3/Match3/Assets/Scripts/NodePiece.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NodePiece : MonoBehaviour
+{
+    public int value;
+    public Point index;
+
+    [HideInInspector]
+    public Vector2 pos;
+    [HideInInspector]
+    public RectTransform rect;
+
+    Image img;
+
+    public void Initialize(int v, Point p, Sprite piece)
+    {
+        img = GetComponent<Image>();
+        rect = GetComponent<RectTransform>();
+        value = v;
+        SetIndex(p);
+        img.sprite = piece;
+    }
+
+    public void SetIndex(Point p)
+    {
+        index = p;
+        ResetPosition();
+        UpdateName();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetPosition()
+    {
+        pos = new Vector2(32 + (64 * index.x), -32 - (64 * index.y));
+    }
+
+    void UpdateName()
+    {
+        transform.name = "Node [" + index.x + ", " + index.y + "]";
+    }
+
+}
