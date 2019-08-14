c773c233f10c252bb6f2029686b1ee0e74ddfd3b
arxcdr <34399111+arxcdr@users.noreply.github.com>
2019-08-14 18:29:30 +0200

    - Update scripts to enable flipping pieces and removing them when a match is detected

--- Match3/Match3/Assets/Scripts/Match3.cs
+++ Match3/Match3/Assets/Scripts/Match3.cs
@@ -17,6 +17,9 @@ public class Match3 : MonoBehaviour
     int height = 14;
     Node[,] board;
 
+    List<NodePiece> update;
+    List<FlippedPieces> flipped;
+
     System.Random random;
 
     // Start is called before the first frame update
@@ -25,10 +28,81 @@ public class Match3 : MonoBehaviour
         StartGame();
     }
 
+    void Update()
+    {
+        List<NodePiece> finishedUpdating = new List<NodePiece>();
+        for (int i = 0; i < update.Count; i++)
+        {
+            NodePiece piece = update[i];
+            if (!piece.UpdatePiece()) finishedUpdating.Add(piece);
+        }
+        for (int i = 0; i < finishedUpdating.Count; i++)
+        {
+            NodePiece piece = finishedUpdating[i];
+            FlippedPieces flip = getFlipped(piece);
+            NodePiece flippedPiece = null;
+            List<Point> connected = isConnected(piece.index, true);
+            bool wasFlipped = (flip != null);
+
+            // If we flipped to make this update
+            if (wasFlipped)
+            {
+                flippedPiece = flip.getOtherPiece(piece);
+                AddPoints(ref connected, isConnected(flippedPiece.index, true));
+            }               
+
+            // If we didn't make a match
+            if(connected.Count == 0)
+            {
+                // If we flipped
+                if(wasFlipped)
+                {
+                    // Flip back
+                    FlipPieces(piece.index, flippedPiece.index, false);
+                }
+            }
+
+            // If we made a match
+            else
+            {
+                // Remove the node pieces connected
+                foreach(Point pnt in connected)
+                {
+                    Node node = getNodeAtPoint(pnt);
+                    NodePiece nodePiece = node.getPiece();
+                    if(nodePiece != null)
+                    {
+                        nodePiece.gameObject.SetActive(false);
+                    }
+                }
+            }
+            // Remove the flip after update
+            flipped.Remove(flip);
+            update.Remove(piece);
+        }
+    }
+
+    FlippedPieces getFlipped(NodePiece p)
+    {
+        FlippedPieces flip = null;
+        for(int i = 0; i < flipped.Count; i++)
+        {
+            if (flipped[i].getOtherPiece(p) != null)
+            {
+                flip = flipped[i];
+                break;
+            }
+                
+        }
+        return flip;
+    }
+
     void StartGame()
     {
         string seed = getRandomSeed();
         random = new System.Random(seed.GetHashCode());
+        update = new List<NodePiece>();
+        flipped = new List<FlippedPieces>();
 
         InitializeBoard();
         VerifyBoard();
@@ -80,15 +154,49 @@ public class Match3 : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                int val = board[x, y].value;
+                Node node = getNodeAtPoint(new Point(x, y));
+                int val = node.value;
                 if (val <= 0) continue;
                 GameObject p = Instantiate(nodePiece, gameBoard);
-                NodePiece node = p.GetComponent<NodePiece>();
+                NodePiece piece = p.GetComponent<NodePiece>();
                 RectTransform rect = p.GetComponent<RectTransform>();
                 rect.anchoredPosition = new Vector2(32 + (64 * x), -32 - (64 * y));
-                node.Initialize(val, new Point(x, y), pieces[val - 1]);
+                piece.Initialize(val, new Point(x, y), pieces[val - 1]);
+                node.SetPiece(piece);
             }
         }
+    }
+
+    public void ResetPiece(NodePiece piece)
+    {
+        piece.ResetPosition();
+        update.Add(piece);
+    }
+
+    public void FlipPieces(Point one, Point two, bool main)
+    {
+        // If it's a hole, do nothing
+        if (getValueAtPoint(one) < 0) return;
+
+        Node nodeOne = getNodeAtPoint(one);
+        NodePiece pieceOne = nodeOne.getPiece();
+        if (getValueAtPoint(two) > 0)
+        {
+            Node nodeTwo = getNodeAtPoint(two);
+            NodePiece pieceTwo = nodeTwo.getPiece();
+
+            // Flip the pieces
+            nodeOne.SetPiece(pieceTwo);
+            nodeTwo.SetPiece(pieceOne);
+
+            if(main)
+                flipped.Add(new FlippedPieces(pieceOne, pieceTwo));
+
+            update.Add(pieceOne);
+            update.Add(pieceTwo);
+        }
+        else
+            ResetPiece(pieceOne);
     }
 
     List<Point> isConnected(Point p, bool main)
@@ -244,6 +352,11 @@ public class Match3 : MonoBehaviour
         board[p.x, p.y].value = v;
     }
 
+    Node getNodeAtPoint(Point p)
+    {
+        return board[p.x, p.y];
+    }
+
     int newValue(ref List<int> remove)
     {
         List<int> available = new List<int>();
@@ -259,12 +372,6 @@ public class Match3 : MonoBehaviour
         return available[random.Next(0, available.Count)];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     // Build a 20-char random seed
     string getRandomSeed()
     {
@@ -278,6 +385,11 @@ public class Match3 : MonoBehaviour
 
         return seed;
     }
+
+    public Vector2 getPositionFromPoint(Point p)
+    {
+        return new Vector2(32 + (64 * p.x), -32 - (64 * p.y));
+    }
 }
 
 [System.Serializable]
@@ -286,12 +398,47 @@ public class Node
 {
     // 0 = blank, 1 = cube, 2 = sphere, 3 = cylinder, 4 = pyramid, 5 = diamond, -1 = hole
     public int value;
-
     public Point index;
+    NodePiece piece;
 
     public Node(int v, Point i)
     {
         value = v;
         index = i;
+    }
+
+    public void SetPiece(NodePiece p)
+    {
+        piece = p;
+        value = (piece == null) ? 0 : piece.value;
+        if (piece == null) return;
+        piece.SetIndex(index);
+    }
+    public NodePiece getPiece()
+    {
+        return piece;
+    }
+}
+
+[System.Serializable]
+public class FlippedPieces
+{
+    public NodePiece one;
+    public NodePiece two;
+
+    public FlippedPieces(NodePiece o, NodePiece t)
+    {
+        one = o;
+        two = t;
+    }
+
+    public NodePiece getOtherPiece(NodePiece p)
+    {
+        if (p == one)
+            return two;
+        else if (p == two)
+            return one;
+        else
+            return null;
     }
 }
--- Match3/Match3/Assets/Scripts/NodePiece.cs
+++ Match3/Match3/Assets/Scripts/NodePiece.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class NodePiece : MonoBehaviour
+public class NodePiece : MonoBehaviour,IPointerDownHandler, IPointerUpHandler
 {
     public int value;
     public Point index;
@@ -12,6 +13,8 @@ public class NodePiece : MonoBehaviour
     public Vector2 pos;
     [HideInInspector]
     public RectTransform rect;
+
+    bool updating;
 
     Image img;
 
@@ -48,9 +51,46 @@ public class NodePiece : MonoBehaviour
         pos = new Vector2(32 + (64 * index.x), -32 - (64 * index.y));
     }
 
+    public void MovePosition(Vector2 move)
+    {
+        rect.anchoredPosition += move * Time.deltaTime * 16f;
+    }
+
+    public void MovePositionTo(Vector2 move)
+    {
+        rect.anchoredPosition = Vector2.Lerp(rect.anchoredPosition, move, Time.deltaTime * 16f);
+    }
+
+    public bool UpdatePiece()
+    {
+        if(Vector3.Distance(rect.anchoredPosition, pos) > 1)
+        {
+            MovePositionTo(pos);
+            updating = true;
+            return true;
+        }
+        else
+        {
+            rect.anchoredPosition = pos;
+            updating = false;
+            return false;
+        }
+    }
+
     void UpdateName()
     {
         transform.name = "Node [" + index.x + ", " + index.y + "]";
     }
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // If we're already grabbing a piece, don't grab another one
+        if (updating) return;
+        MovePieces.instance.MovePiece(this);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        MovePieces.instance.DropPiece();
+    }
 }
